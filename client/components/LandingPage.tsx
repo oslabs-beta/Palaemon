5494befba026f9cb757607e49cf73d65baac3e75
Si Young Mah <siyoungmah@gmail.com>
2022-09-16 20:07:05 -0700

    Save namespace selection in localStorage
    LandingPage is now fully functional and will reload

--- client/components/LandingPage.tsx
+++ client/components/LandingPage.tsx
@@ -1,22 +1,34 @@
 import { useState, useEffect } from "react";
-import { ClusterChartProps, SvgInfo, ModalProps, Lulu } from "../Types";
+import { useNavigate } from "react-router-dom";
+
 
 const LandingPage = ():JSX.Element => {
+  const [namespaces, setNamespaces] = useState([]);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    window.api.getNamespaces()
+      .then((data) => {setNamespaces(data)})
+      .catch((err:Error) => {console.log('An error occurred in LandingPage in function useEffect() for namespaces')
+    });
+  }, [namespaces]);
 
-  const getNamespaces = async () => {
-    const namespaces = await window.api.getNamespaces();
-    console.log('I am namespaces ', namespaces);
+  const handleNamespaceChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    localStorage.setItem('namespace', e.target.value);
+    navigate('/home');
   }
-
-  getNamespaces();
 
   return (
       <div id="landing-container">
         <form id="namespace-selector-form">
           <h2>Choose a namespace to get started</h2>
-          <select name="namespace-select" id="selector-namespace" defaultValue={""}>
+          <select name="namespace-select" 
+                  id="selector-namespace" 
+                  defaultValue={""}
+                  onChange={e => {handleNamespaceChange(e)}}
+                  >
             <option value="" disabled hidden>Choose a namespace</option>
-            <option value=""></option>
+            {namespaces.map((namespace) => <option value={namespace}>{namespace}</option>)}
           </select>
         </form>
         <img id="logo" src="./assets/logo.png" alt="" />
