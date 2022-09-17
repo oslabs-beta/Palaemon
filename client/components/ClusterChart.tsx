822ff42b8b504d65156898ac5e15ca9e599280e6
Patrick <patrickhu91@gmail.com>
2022-09-17 11:45:51 -0700

    add comments for cluster chart usage feature

--- client/components/ClusterChart.tsx
+++ client/components/ClusterChart.tsx
@@ -8,11 +8,14 @@ const ClusterChart = (props: ClusterChartProps): JSX.Element => {
   const names: ["Clusters", "Nodes", "Pods", "Deployments"] = ["Clusters", "Nodes", "Pods", "Deployments"];
   // const names: string[] = ["Clusters", "Nodes", "Pods", "Deployments",'a','a'];
   const clusterCards: JSX.Element[] = [];
+
+  // invoke add  usage function to element on props[names[i]][length-1]
   if (props.click)
     for (let i = 0; i < 4; i++) {
       clusterCards.push(
         <ClusterChartCard
           title={names[i]}
+          // function to check if last data element.. then add usage
           data={props[names[i]]}
           click={props.click}
           close={props.close}
